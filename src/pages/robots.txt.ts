55e9c2b78fe09a589cf53a10b157de3f1579d099
natekeem <natekeem@users.noreply.github.com>
2026-07-16 00:46:23 +0900

    chore: harden project-site search readiness

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -8,6 +8,7 @@ Sitemap: ${sitemapURL.href}
 `;
 
 export const GET: APIRoute = ({ site }) => {
-  const sitemapURL = new URL("sitemap-index.xml", site);
+  const basePath = import.meta.env.BASE_URL.replace(/\/?$/, "/");
+  const sitemapURL = new URL(`${basePath}sitemap-index.xml`, site);
   return new Response(getRobotsTxt(sitemapURL));
 };
